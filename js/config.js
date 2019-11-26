12ccdb0ed0d6864555d0489b71725ab648ca18ef
gitajh360 <57452921+gitajh360@users.noreply.github.com>
2019-11-27 01:22:40 +0800

    1127

---
+++ js/config.js
@@ -0,0 +1,24 @@
+sd_io = document.getElementById("sound");
+
+sd_io.onclick = function(){
+if(sound==true){
+sd_io.innerHTML = '<img src="images/no-sound.png">';
+sound=!sound;
+}else{
+sd_io.innerHTML = '<img src="images/sound.png">';
+sound=!sound;
+}
+
+}
+
+sd_io_2 = document.getElementById("sd2");
+sd_io_2.onclick = function(){
+if(sound==true){
+sd_io_2.innerHTML = '<img src="images/no-sound.png"><div>声音</div>';
+sound=!sound;
+}else{
+sd_io_2.innerHTML = '<img src="images/sound.png"><div>静音</div>';
+sound=!sound;
+}
+
+}
